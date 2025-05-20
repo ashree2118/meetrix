aa5122a53ba7ab342a989ca6650cf834e133c2b5
ashree2118 <ashree2118@gmail.com>
2025-05-20 20:54:33 +0530

    feat: add user authentication with JWT and bcrypt, implement user registration endpoint

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from mongoose;
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     userId: {
@@ -37,5 +39,37 @@ const userSchema = new Schema({
     },
 });
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    //only encrypt password when password field is updated
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            name: this.name,
+        },
+        process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,{
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)
