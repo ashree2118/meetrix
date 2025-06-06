623e7cab87a8fdef1d42fa999fa6f53e5b8f00a8
ashree2118 <ashree2118@gmail.com>
2025-06-06 23:47:40 +0530

    fix: comment out response message in registerUser function and extract user data from request body

--- server/src/contollers/user.controller.js
+++ server/src/contollers/user.controller.js
@@ -2,7 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({message: "ok"})
+    // res.status(200).json({message: "ok"})
+    const {email, password, name} = req.body
 })
 
 
