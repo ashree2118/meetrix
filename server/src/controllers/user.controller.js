67f18d6c9445f66ce53da2b8fcef459f831d675c
ashree2118 <ashree2118@gmail.com>
2025-06-16 23:13:47 +0530

    feat: add password change, current user retrieval, and account update functionalities in user controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -157,6 +157,43 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     } catch (error) {
         throw new ApiError(401, error?.message || "Something went wrong while refreshing access token")
     }
+
 })
 
-export {registerUser, loginUser, logoutUser} 
+const changeCurrentPassword = asyncHandler(async(req, res) =>{
+    const {oldPassword, newPassword} = req.body
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password")
+    }
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+    return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res.status(200).json(200, req.user, "current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {name, email, timezone} = req.body
+    const user = await User.findByIdAndUpdate(req.user._id,{
+        $set: {
+            name,
+            email,
+            timezone
+        }
+    }, {new: true}).select("-password -refreshToken")
+
+    if(!user){
+        throw new ApiError(404, "User not found")
+    }
+
+    await user.save({validateBeforeSave: false})
+
+    return res.status(200).json(new ApiResponse(200, user, "User details updated successfully"))
+})
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails}
