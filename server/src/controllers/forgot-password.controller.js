d36f6239e19be27cbe5c0a263428a289de0f8308
ashree2118 <ashree2118@gmail.com>
2025-08-20 20:51:40 +0530

    brevo fix-2

--- server/src/controllers/forgot-password.controller.js
+++ server/src/controllers/forgot-password.controller.js
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 import { User } from "../models/user.model.js";
-import sendEmail from "../services/emailService.js"; // use Nodemailer/Brevo
+import sendMeetingConfirmation from "../services/emailService.js";
 
 export const forgotPassword = async (req, res) => {
   try {
@@ -20,12 +20,23 @@ export const forgotPassword = async (req, res) => {
     await user.save();
 
     const resetUrl = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
-    const message = `You requested a password reset. Click here: ${resetUrl}`;
+    const message = `
+      <p>You requested a password reset.</p>
+      <p>Click below to reset your password:</p>
+      <a href="${resetUrl}" style="color:#4F46E5; font-weight:bold;">Reset Password</a>
+      <p>This link will expire in 15 minutes.</p>
+    `;
 
-    await sendEmail(user.email, "Password Reset", message);
+    await sendMeetingConfirmation({
+      toEmail: user.email,
+      toName: user.name || "",
+      subject: "Password Reset Request",
+      content: message,
+    });
 
     res.json({ message: "Password reset link sent to your email." });
   } catch (err) {
+    console.error("Forgot password error:", err.message);
     res.status(500).json({ message: "Server error" });
   }
 };
